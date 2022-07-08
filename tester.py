c3ed1b87a058aa2eead6499a168603c7579474bc
Harish Akula <harish.akula32k@gmail.com>
2022-07-08 12:21:12 +0000

    updated OFA repo removed .git in OFA folder

--- tester.py
+++ tester.py
@@ -2,7 +2,7 @@ from json_conv import pred_dict
 import json
 import requests
 import tester
-url = 'http://0.0.0.0:8080/predict'
-r = requests.get(url, json=pred_dict)
+url = 'http://127.0.0.1:5000/predict'
+r = requests.post(url, json=pred_dict)
 
 print(r.json())
